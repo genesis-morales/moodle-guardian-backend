381c9a703bbeaf6a30616cf0054278c79d152a88
Génesis Morales <genesismoralesc1@gmail.com>
2026-06-14 17:08:11 -0600

    Update health.py

--- src/api/v1/health.py
+++ src/api/v1/health.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Response
 
 router = APIRouter(prefix="/v1/health", tags=["Health"])
 
-@router.get("", status_code=status.HTTP_200_OK)
-async def healthcheck() -> dict[str, str]:
-    return {"status": "ok"}
+@router.api_route("", methods=["GET", "HEAD"], status_code=status.HTTP_200_OK, include_in_schema=False)
+async def healthcheck() -> Response:
+    return Response(content='{"status":"ok"}', media_type="application/json")
